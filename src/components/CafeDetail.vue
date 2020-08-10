5bde193027a0ebaac0ab20f7093e61f971765caa
wendy.hsiao <wendy19940331@gmail.com>
2020-08-10 15:00:07 +0800

    【加入】cafe 頁加入畫廊套件，安裝 vue-slick-carousel

--- src/components/CafeDetail.vue
+++ src/components/CafeDetail.vue
@@ -1,22 +1,4 @@
 <template>
-  <div class="card mb-3">
-    <div class="row no-gutters">
-      <div class="col-md-6 p-3">
-        <img 
-          v-if="cafe.Images.length > 0"
-          :src="cafe.Images[0].url" class="img-main">
-        <div>
-          <ul class="pl-0 mb-0">
-            <li v-for="image in cafe.Images"
-              :key="image.id"
-              class="img-list pr-3 pt-3 w-25"
-              >
-              <img :src="image.url">
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-6">
         <div class="card-body">
           <h1 class="card-title">{{cafe.name}}</h1>
           <p class="card-text">連話電話 {{cafe.tel}}</p>
@@ -25,11 +7,7 @@
           <p class="card-text">最低消費 {{cafe.minimum_charge}}</p>
           <p class="card-text">Facebook {{cafe.facebook}}</p>
           <p class="card-text">Instagram {{cafe.instagram}}</p>
-          <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
         </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -42,14 +20,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.img-main, .img-list>img {
-  max-height: auto;
-  width: 100%;
-}
-.img-list {
-  list-style-type: none;
-  display: inline-block;
-}
-</style>
