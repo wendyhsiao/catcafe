0c21303c423eefcd66d147a8d02453c06f85e328
wendy.hsiao <wendy19940331@gmail.com>
2020-08-14 00:46:25 +0800

    【加入】cafes 頁無限下拉分頁，安裝 vue-infinite-loading

--- src/apis/cafes.js
+++ src/apis/cafes.js
@@ -1,8 +1,8 @@
 import { apiHelper } from '../utils/helpers.js'
 
 export default {
-  getCafes ({ search }) {
-    const searchParams = new URLSearchParams(search)
+  getCafes ({ search, page }) {
+    const searchParams = new URLSearchParams({ search, page })
     return apiHelper.get(`/cafes?${searchParams.toString()}`)
   },
   getCafe (id) {
