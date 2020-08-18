b8aa6192807f54b60c6d48deda3fd823b92cacbb
wendy.hsiao <wendy19940331@gmail.com>
2020-08-18 11:26:53 +0800

    【優化】後台 css 網頁樣式

--- src/components/admin/AdminCafeList.vue
+++ src/components/admin/AdminCafeList.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <form class="form-inline">
+    <form class="form-inline mb-3">
       <input type="search" name="search" 
         v-model="search"
         class="form-control mr-sm-2" placeholder="Search name">
       <button 
         @click.stop.prevent="handleSubmit"
-        class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
     </form>
 
     <table  class="table table-bordered table-hover">
